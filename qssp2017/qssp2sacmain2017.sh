1efc1842f20270d4ab364d19a43e171df5809196
shenjianzhang <sjzhang@pku.edu.cn>
2018-07-20 21:46:34 +0200

    main.sh for qssp2sac2017

---
+++ qssp2017/qssp2sacmain2017.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This is a bash script to make SAC files from output files
+# of qssp.
+#
+# Author:
+#       Shenjian Zhang,    sjzhang@pku.edu.cn
+# Input:
+#       name of input file for qssp
+#       name of target file(qssp output file)
+# Inside program:
+#       mkhead    # write head info. into datafiles
+#       qssp2sac  # make SAC files according to datafiles
+# Run:
+#       ./qssp2sacmain.sh qsspinp qsspout
+
+
+# prepare head info.
+qsspinp=$1    # inp. file of qssp
+qsspout=$2    # outp. file of qssp
+
+echo $qsspinp > mkhead.inp
+echo $qsspout >> mkhead.inp
+
+#rm -rf ./datafiles
+mkdir ./datafiles
+mkdir ./sacfiles 
+
+NR=`./mkhead2017 < mkhead.inp`    # the number of receivers
+i=1
+while((i <= ($NR)))
+do
+    datafile=$i.$qsspout
+    let "j=i+1"
+    # write data into datafiles
+    awk '{print $1,$'$j'}' $qsspout >> ./datafiles/$datafile
+    echo \"./datafiles/$datafile\" > qssp2sac.inp
+    ./qssp2sac2017 < qssp2sac.inp
+    let "i += 1"
+done
+
